a9857b6802477213d84ecd150983088ecd6660a0
jungai <juworaphol125@gmail.com>
2021-04-10 19:44:56 +0700

    feat: add landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
+import Profile from "../components/Profile";
+import Posts from "../components/Posts";
 
 const IndexPage: FC = () => (
-  <div className="text-red-300 underline">
-    <h1>Hello Nextjs with tailwind</h1>
+  <div className="grid grid-col-1 md:grid-cols-2 max-w-5xl mx-auto min-h-screen items-start box-border">
+    <Profile />
+    <div>
+      <Posts />
+    </div>
   </div>
 );
 
