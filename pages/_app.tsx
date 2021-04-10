5c28592ce0a09ba6493870ebe603f609f97b288c
jungai <juworaphol125@gmail.com>
2021-04-10 17:32:00 +0700

    chore: init nextjs with tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../styles/tailwind.css";
+import { FC } from "react";
+import { AppProps } from "next/app";
+
+export const App: FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const IndexPage: FC = () => (
+  <div className="text-red-300 underline">
+    <h1>Hello Nextjs with tailwind</h1>
+  </div>
+);
+
+export default IndexPage;
