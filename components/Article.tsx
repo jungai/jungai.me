b42d274bcc2f8d278c07724ca192d63a988c92a6
jungai <juworaphol125@gmail.com>
2021-04-17 14:26:01 +0700

    fix: overflow should auto

--- components/Article.tsx
+++ components/Article.tsx
@@ -5,9 +5,9 @@ import Posts from "./Posts";
 const Article: FC<{ posts: IKeyDataFromMatter[] }> = ({ posts }) => (
 	<div className="p-4 md:p-0">
 		<h1 className="text-2xl underline font-semibold text-light-head mb-2">
-			Blogs
+			Posts
 		</h1>
-		<div className="overflow-y-scroll max-h-80">
+		<div className="overflow-auto max-h-80">
 			<Posts posts={posts} />
 		</div>
 	</div>
