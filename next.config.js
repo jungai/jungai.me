d615c064baa7eaf5ed6db44087d8241f99a436fd
jungai <juworaphol125@gmail.com>
2021-05-13 18:31:40 +0700

    perf: use webpack5

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withMDX = require("@next/mdx")({
 });
 
 module.exports = {
+	future: {
+    webpack5: true,
+  },
 	...withMDX({
 		pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	}),
