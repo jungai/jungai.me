7664341a9b47c971df2989369fcb06cb4bbb6eb2
jungai <juworaphol125@gmail.com>
2021-05-26 14:22:55 +0700

    refactor: add padding

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 
 const LayoutDefault: FC = ({ children }) => (
 	<div className="h-screen md:flex md:justify-center md:items-center">
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-4">{children}</div>
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:px-4">
+			{children}
+		</div>
 	</div>
 );
 
