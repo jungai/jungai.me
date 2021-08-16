d1a546171ea1d4766a5b0bd1071925816d1d5c69
jungai <juworaphol125@gmail.com>
2021-08-17 01:56:57 +0700

    sizing

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,9 @@ import { ReactElement } from "react";
 export function Loading(): ReactElement {
 	return (
 		<div className="h-screen w-full grid place-items-center">
-			<p className="text-light-head uppercase">loading ...</p>
+			<p className="text-3xl md:text-6xl text-light-head uppercase">
+				loading ...
+			</p>
 		</div>
 	);
 }
