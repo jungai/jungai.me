4dd00729ab1743f00841e222ddf24e74365f75db
jungai <juworaphol125@gmail.com>
2022-01-29 13:59:37 +0700

    remove border

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,11 +2,7 @@ import { FC } from 'react';
 import { IContacts } from '@/components/Contacts';
 
 const Contact: FC<IContacts> = ({ link, href, icon, msg }) => (
-	<li
-		className={`mr-2 border p-2 rounded-lg hover:border-2 hover:border-light-head  hover:text-light-head ${
-			link ? 'cursor-pointer' : ''
-		}`}
-	>
+	<li className={`mr-2 rounded-lg hover:text-light-head ${link ? 'cursor-pointer' : ''}`}>
 		{link ? (
 			<a href={href} target="_blank" className="grid place-items-center" rel="noreferrer">
 				<span>{icon}</span>
