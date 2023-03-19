3f5f2cc19983d28e5db6f4c890a11b1ac41b2c1c
jungai <juworaphol125@gmail.com>
2023-03-19 16:45:26 +0700

    ci(app): update tailwind config

--- apps/dev/tailwind.config.cjs
+++ apps/dev/tailwind.config.cjs
@@ -1,7 +1,15 @@
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bg: "#072448",
+				primary: "#54D2D2",
+				secondary: "#F8AA4B",
+				warning: "#FFCB00",
+				error: "#ff6150",
+			},
+		},
 	},
 	plugins: [],
 };
